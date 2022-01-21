c0f63c34be5c087ef78e5df61d9bd39fae2de3fb
Stephen Allard <stephencallard@gmail.com>
2022-01-20 21:14:27 -0600

    display weather in celsius or fahrenheit

--- src/display-weather.js
+++ src/display-weather.js
@@ -1,35 +1,73 @@
 const weatherContainer = document.querySelector('#weather');
 
 function displayWeather(weatherData) {
-  let name = document.createElement('p');
-  let country = document.createElement('p')
-  let desc = document.createElement('p');
-  let temp = document.createElement('p');
-  let feelsLike = document.createElement('p');
-  let tempMax = document.createElement('p');
-  let tempMin = document.createElement('p');
-  let windSpeed = document.createElement('p');
-  let windDirection = document.createElement('p');
+  let nameValue = document.createElement('p');
 
-  name.innerHTML = weatherData.name;
-  country.innerHTML = weatherData.country;
-  desc.innerHTML = weatherData.desc;
-  temp.innerHTML = weatherData.temp;
-  feelsLike.innerHTML = weatherData.feelsLike;
-  tempMax.innerHTML = weatherData.tempMax;
-  tempMin.innerHTML = weatherData.tempMin;
-  windSpeed.innerHTML = weatherData.windSpeed;
-  windDirection.innerHTML = weatherData.windDir;
+  let countryValue = document.createElement('p');
 
-  weatherContainer.appendChild(name);
-  weatherContainer.appendChild(country);
-  weatherContainer.appendChild(desc);
-  weatherContainer.appendChild(temp);
-  weatherContainer.appendChild(feelsLike);
-  weatherContainer.appendChild(tempMax);
-  weatherContainer.appendChild(tempMin);
-  weatherContainer.appendChild(windSpeed);
-  weatherContainer.appendChild(windDirection);
+  let descValue = document.createElement('p');
+
+  let tempValue = document.createElement('p');
+
+  let feelsLikeLabel = document.createElement('p');
+  let feelsLikeValue = document.createElement('p');
+
+  let tempMaxLabel = document.createElement('p');
+  let tempMaxValue = document.createElement('p');
+
+  let tempMinLabel = document.createElement('p');
+  let tempMinValue = document.createElement('p');
+
+  let windLabel = document.createElement('p');
+  let windSpeedValue = document.createElement('p');
+  let windDirectionValue = document.createElement('p');
+  let tempUnits = document.createElement('p');
+  let windSpeedUnits = document.createElement('p');
+
+  nameValue.innerHTML = weatherData.name;
+  countryValue.innerHTML = weatherData.country;
+  descValue.innerHTML = weatherData.desc;
+  tempValue.innerHTML = weatherData.temp;
+  tempValue.classList.add('temp-value');
+  
+  feelsLikeLabel.innerHTML = 'Feels Like: ';
+  feelsLikeValue.innerHTML = weatherData.feelsLike;
+  feelsLikeValue.classList.add('temp-value');
+
+  tempMaxLabel.innerHTML = 'High: ';
+  tempMaxValue.innerHTML = weatherData.tempMax;
+  tempMaxValue.classList.add('temp-value');
+
+  tempMinLabel.innerHTML = 'Low: ';
+  tempMinValue.innerHTML = weatherData.tempMin;
+  tempMinValue.classList.add('temp-value');
+
+  windLabel.innerHTML = 'Wind: ';
+  windSpeedValue.innerHTML = weatherData.windSpeed ;
+  windDirectionValue.innerHTML = weatherData.windDir;
+
+  tempUnits.innerHTML = 'F';
+  tempUnits.classList.add('temp-unit');
+  windSpeedUnits.innerHTML = 'mph';
+
+  weatherContainer.appendChild(nameValue);
+  weatherContainer.appendChild(countryValue);
+  weatherContainer.appendChild(descValue);
+  weatherContainer.appendChild(tempValue);
+  weatherContainer.appendChild(tempUnits);
+  weatherContainer.appendChild(feelsLikeLabel);
+  weatherContainer.appendChild(feelsLikeValue);
+  weatherContainer.appendChild(tempUnits.cloneNode(true));
+  weatherContainer.appendChild(tempMaxLabel);
+  weatherContainer.appendChild(tempMaxValue);
+  weatherContainer.appendChild(tempUnits.cloneNode(true));
+  weatherContainer.appendChild(tempMinLabel);
+  weatherContainer.appendChild(tempMinValue);
+  weatherContainer.appendChild(tempUnits.cloneNode(true));
+  weatherContainer.appendChild(windLabel);
+  weatherContainer.appendChild(windSpeedValue);
+  weatherContainer.appendChild(windSpeedUnits);
+  weatherContainer.appendChild(windDirectionValue);
 }
 
-export default displayWeather
+export default displayWeather;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import getWeather from './get-weather.js';
 import displayWeather from './display-weather.js';
 
 const form = document.querySelector('form');
+const celsiusButton = document.querySelector('#celsius');
+const fahrenheitButton = document.querySelector('#fahrenheit');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -19,3 +21,44 @@ form.addEventListener('submit', (event) => {
     })
 });
 
+celsiusButton.addEventListener('click', () => {
+  const tempValueNodes = document.querySelectorAll('.temp-value');
+  const tempUnitNodes = document.querySelectorAll('.temp-unit');
+  
+  if (fahrenheitButton.classList.contains('active')) {
+    // convert values to celsius
+    tempValueNodes.forEach( (node) => {
+      node.innerHTML = ((parseFloat(node.innerHTML) - 32 ) / 1.8).toFixed(2);
+    });
+
+    // change units to C
+    tempUnitNodes.forEach( (node) => {
+      node.innerHTML = 'C';
+    });
+
+    // transfer '.active' from the fahrenheit button to the celsius button
+    fahrenheitButton.classList.toggle('active');
+    celsiusButton.classList.toggle('active');
+  }
+});
+
+fahrenheitButton.addEventListener('click', () => {
+  const tempValueNodes = document.querySelectorAll('.temp-value');
+  const tempUnitNodes = document.querySelectorAll('.temp-unit');
+  
+  if (celsiusButton.classList.contains('active')) {
+    // convert values to fahrenheit
+    tempValueNodes.forEach( (node) => {
+      node.innerHTML = ((parseFloat(node.innerHTML) * (9/5)) + 32).toFixed(2);
+    });
+
+    // change units to F
+    tempUnitNodes.forEach( (node) => {
+      node.innerHTML = 'F';
+    });
+
+    // transfer '.active' from the celsius button to the fahrenheit button
+    fahrenheitButton.classList.toggle('active');
+    celsiusButton.classList.toggle('active');
+  }
+});
